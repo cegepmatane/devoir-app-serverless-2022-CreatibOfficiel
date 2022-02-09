d5e2c2208f279d09f49bf68d5470eec865b56fe4
Mathéo <matheo.galu56@gmail.com>
2022-02-09 17:56:43 -0500

    button style & added mathéo's API endpoints

--- app/apiURL.js
+++ app/apiURL.js
@@ -1,5 +1,13 @@
+// Thibaud
+// const apiUrl = {
+//     "getAll": "https://3ogj7wx99l.execute-api.us-east-1.amazonaws.com/default/list-recipe-function",
+//     "getById": "https://3ogwdphqf2.execute-api.us-east-1.amazonaws.com/default/chercher-par-id-recipe-function",
+//     "add": "https://tr4mkn0sfh.execute-api.us-east-1.amazonaws.com/default/add-recipe-function"
+// };
+
+// Mathéo
 const apiUrl = {
-    "getAll": "https://3ogj7wx99l.execute-api.us-east-1.amazonaws.com/default/list-recipe-function",
-    "getById": "https://3ogwdphqf2.execute-api.us-east-1.amazonaws.com/default/chercher-par-id-recipe-function",
-    "add": "https://tr4mkn0sfh.execute-api.us-east-1.amazonaws.com/default/add-recipe-function"
+    "getAll": "https://w15l83753b.execute-api.us-east-1.amazonaws.com/default/getAll",
+    "getById": "https://a7znd78qi6.execute-api.us-east-1.amazonaws.com/default/getById",
+    "add": "https://5m9x1op4w3.execute-api.us-east-1.amazonaws.com/default/add"
 };
---
+++ lambda/getByIdRecipe/matheo.js
@@ -0,0 +1,41 @@
+console.log('Loading function');
+
+var AWS = require('aws-sdk');
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+exports.handler = async (event) => {
+    const id = event.queryStringParameters && event.queryStringParameters.id;
+
+    let response = {
+        statusCode: 400,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: 'You must give a recipe id'
+    };
+    if (id == null) {
+        return response;
+    }
+
+    const params = {
+        Bucket: "recipebook-matheo",
+        Key: "storage.json",
+    };
+
+    const data = await s3.getObject(params).promise();
+    console.log("Raw text:\n" + data.Body.toString('utf-8'));
+    const listRecipeJson = data.Body.toString('utf-8');
+    const listRecipe = JSON.parse(listRecipeJson);
+
+    let recipe = listRecipe.find(recipe => recipe.id == id);
+
+    response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(recipe).toString('utf-8')
+    };
+
+    return response;
+};
