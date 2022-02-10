6a73c5475de0246ec063d075bdda8613d0d2eb11
clement_ghn <ent.clementgehin@gmail.com>
2022-02-10 16:06:54 -0500

    commit

--- app/apiURL.js
+++ app/apiURL.js
@@ -5,9 +5,17 @@
 //     "add": "https://tr4mkn0sfh.execute-api.us-east-1.amazonaws.com/default/add-recipe-function"
 // };
 
-// Mathéo
+/*Mathéo
 const apiUrl = {
     "getAll": "https://w15l83753b.execute-api.us-east-1.amazonaws.com/default/getAll",
     "getById": "https://a7znd78qi6.execute-api.us-east-1.amazonaws.com/default/getById",
     "add": "https://5m9x1op4w3.execute-api.us-east-1.amazonaws.com/default/add"
+};
+*/
+
+//Clément
+const apiUrl = {
+    "getAll": "https://0o6elo573g.execute-api.us-east-1.amazonaws.com/default/lister-recipe",
+    "getById": "https://lvsp3fboce.execute-api.us-east-1.amazonaws.com/default/chercher-par-id-recipe",
+    "add": "https://2ixxiq6tbi.execute-api.us-east-1.amazonaws.com/default/ajouter-recipe"
 };
